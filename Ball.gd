52c2f4dbaa9ca7268fcbdde6e95aded96e4e64e9
Anders Nissen <andnis@gmail.com>
2019-09-10 22:13:02 +0200

    Added sounds

--- Ball.gd
+++ Ball.gd
@@ -4,4 +4,10 @@ signal block_hit(block)
 
 func _on_Ball_body_entered(body :Node):
 	if body.is_in_group("Blocks"):
+		if body.destructable:
+			$hitBlockSound.play()
+		else:
+			$hitStoneBlockSound.play()
 		emit_signal("block_hit", body)
+	if body.is_in_group("Border"):
+		$hitBorderSound.play()
