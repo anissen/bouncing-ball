f329e2e630eb5cb54a1ba1b73923cc57fe63e87b
Anders Nissen <andnis@gmail.com>
2019-09-19 22:25:48 +0200

    Added aiming slowdown + gravity zone

--- Main.gd
+++ Main.gd
@@ -3,19 +3,27 @@ extends Node2D
 var score := 0
 var swipes := 0
 var elapsed_seconds := 0
-var is_swiped := false
+var is_swiping := false
 
 onready var explosion_scene = preload("res://Explosion.tscn")
 
 func _input(event :InputEvent):
-	if $Ball == null: return
-	if event is InputEventScreenDrag:
-		$Ball.apply_central_impulse(event.relative * 10)
-		is_swiped = true
-	if event.is_action_released("ui_touch") and is_swiped:
-		is_swiped = false
+	if !get_node_or_null("Ball"): return
+
+	if event.is_action_pressed("ui_touch"):
+#		is_swiping = true
+#		var tween = Tween.new()
+#		tween.interpolate_property(Engine, "time_scale", Engine.time_scale, 0.1, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+#		add_child(tween)
+#		tween.start()
+#		#yield(tween, "tween_completed")
+		Engine.time_scale = 0.2
+	elif event.is_action_released("ui_touch"):
+		is_swiping = false
 		swipes += 1
-		update_label()
+		Engine.time_scale = 1
+		var diff = event.position - $Ball.position
+		$Ball.apply_central_impulse(diff * 5)
 
 	if event.is_action_released("restart"):
 		restart()
@@ -26,10 +34,10 @@ func restart():
 func update_label():
 	var time_left = 30 - elapsed_seconds;
 	if time_left < 0: time_left = 0
-	var time_color = "blue" if time_left > 0 else "red"
+	var time_color = "#0074D9" if time_left > 0 else "#FF4136"
 	var swipes_left = 5 - swipes
 	if swipes_left < 0: swipes_left = 0
-	var swipes_color = "blue" if swipes_left > 0 else "red"
+	var swipes_color = "#0074D9" if swipes_left > 0 else "#FF4136"
 	$ObjectiveLabel.bbcode_text = "[center]Time: [color=" + time_color + "]" + str(time_left) + "[/color] / Swipes: [color=" + swipes_color + "]" + str(swipes_left) + "[/color][/center]"
 
 	if time_left == 0 and swipes_left == 0:
@@ -37,9 +45,10 @@ func update_label():
 		get_tree().create_timer(1).connect("timeout", self, "restart")
 
 func _on_Ball_block_hit(block):
-	if !$Ball: return
+	if !get_node_or_null("Ball"): return
 	if !block.destructable: return
 	if block.type == block.Type.EXPLOSIVE:
+		Engine.time_scale = 1
 		$Ball.queue_free()
 		$LevelLostMessage.show()
 		get_tree().create_timer(1).connect("timeout", self, "restart")
@@ -62,7 +71,9 @@ func _on_Ball_block_hit(block):
 			has_won = false
 			break
 
-	if has_won: $LevelWonMessage.show()
+	if has_won:
+		$Ball.queue_free()
+		$LevelWonMessage.show()
 
 func _on_GameTimer_timeout():
 	elapsed_seconds += 1
